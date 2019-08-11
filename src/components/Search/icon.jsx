c3b9680e7143b0c362992b53fbd4a16758aa0c2c
leocantthinkofanamae <i7851@hotmail.com>
2019-08-11 19:30:09 +0800

    add some assets

---
+++ src/components/Search/icon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// img
+import svg from "./search.svg";
+
+const Icon = () => {
+  return <img src={svg} alt="Search" className="search-icon" />;
+};
+
+export default Icon;
